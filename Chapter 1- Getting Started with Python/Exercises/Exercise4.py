6f59cd6140b97715e40b3e7a81c0e8b81e0298a4
Mannan7922 <syedmnax790@gmail.com>
2023-09-27 14:03:03 +0400

    Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+# Define three strings in different variables
+str1 = "Hello, "
+str2 = "world!"
+str3 = " How are you?"
+
+# Concatenate the strings and print the output as one string
+result = str1 + str2 + str3
+print(result)
