f1f13a80cd90508d7fc0bb5f870eb5e1a30edda7
Mannan7922 <syedmnax790@gmail.com>
2023-10-11 07:20:00 +0400

    Practice 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 2.py
@@ -0,0 +1,5 @@
+famous_person = "Steve Jobs"
+
+massage = f' {famous_person} once said, "Have the courge to follow your heart and intuition. They somehow know what you turly want to become." '
+
+print(massage)
