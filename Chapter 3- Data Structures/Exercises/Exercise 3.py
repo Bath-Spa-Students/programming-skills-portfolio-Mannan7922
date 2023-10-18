5bfaf53b6cbc16db45889d737a97cd97f657dfbf
Mannan7922 <syedmnax790@gmail.com>
2023-10-18 21:53:40 +0400

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,9 @@
+# Create a list of favorite modes of transportation
+favorite_transportation = ["motorcycle", "car", "bicycle", "scooter", "skateboard"]
+
+# Use the list to print statements about these items
+for item in favorite_transportation:
+    print(f"I would like to own a {item.capitalize()}.")
+
+# You can also print a general statement about your favorite mode
+print(f"\nOut of all, my absolute favorite mode of transportation is a {favorite_transportation[0]}.")
