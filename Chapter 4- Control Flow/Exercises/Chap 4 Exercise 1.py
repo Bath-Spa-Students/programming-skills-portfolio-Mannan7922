fd5388b7b59f7728b1cc872e743978742dcd877e
Mannan7922 <syedmnax790@gmail.com>
2023-10-25 10:09:33 +0400

    Chap 4  Exercise. 1py

---
+++ Chapter 4- Control Flow/Exercises/Chap 4 Exercise 1.py
@@ -0,0 +1,12 @@
+#Version 1
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+    #Version 2
+    alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
