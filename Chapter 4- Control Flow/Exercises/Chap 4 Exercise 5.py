978501a0efdca3a7aa3a2724aec461730599f0af
Mannan7922 <syedmnax790@gmail.com>
2023-10-28 15:17:23 +0400

    Chap 4 Exercise 5.py
    In this example, the list favorite_fruits contains three fruits. The if statements check for specific fruits in the list and, if the fruit is found in the list, it prints a corresponding message indicating that you really like that fruit.

---
+++ Chapter 4- Control Flow/Exercises/Chap 4 Exercise 5.py
@@ -0,0 +1,14 @@
+# Make a list of favorite fruits
+favorite_fruits = ['apple', 'banana', 'strawberry']
+
+# Check for specific fruits in the list
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+if 'orange' in favorite_fruits:
+    print("You really like oranges!")
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+if 'kiwi' in favorite_fruits:
+    print("You really like kiwis!")
