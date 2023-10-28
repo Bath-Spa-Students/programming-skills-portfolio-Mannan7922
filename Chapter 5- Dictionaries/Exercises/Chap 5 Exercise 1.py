20ccb0663e9ec63c35d7ed482d9fcee8d3746b7c
Mannan7922 <syedmnax790@gmail.com>
2023-10-28 19:30:08 +0400

    Chap 5 Exercise 1.py
    In this code, a dictionary called person is created with keys like 'first_name', 'last_name', 'age', and 'city' storing specific information. The print statements then retrieve and display each piece of information stored in the dictionary. Adjust the values in the dictionary to match the information of the person you want to store.

---
+++ Chapter 5- Dictionaries/Exercises/Chap 5 Exercise 1.py
@@ -0,0 +1,13 @@
+# Create a dictionary to store information about a person
+person = {
+    'first_name': 'Syed',
+    'last_name': 'Ali',
+    'age': 30,
+    'city': 'New York'
+}
+
+# Print each piece of information stored in the dictionary
+print("First Name:", person['first_name'])
+print("Last Name:", person['last_name'])
+print("Age:", person['age'])
+print("City:", person['city'])
