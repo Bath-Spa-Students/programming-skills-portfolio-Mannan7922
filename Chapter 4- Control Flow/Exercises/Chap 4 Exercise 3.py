b802efe70875f59135d47569d4ec453de3c097a4
Mannan7922 <syedmnax790@gmail.com>
2023-10-28 15:09:46 +0400

    Chap 4 Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/Chap 4 Exercise 3.py
@@ -0,0 +1,29 @@
+#Version 1 (Green alien - 5 points):
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the green alien.")
+elif alien_color == 'yellow':
+    print("You just earned 10 points for shooting the yellow alien.")
+else:
+    print("You just earned 15 points for shooting the red alien.")
+
+#Version 2 (Yellow alien - 10 points):
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the green alien.")
+elif alien_color == 'yellow':
+    print("You just earned 10 points for shooting the yellow alien.")
+else:
+    print("You just earned 15 points for shooting the red alien.")
+
+#Version 3 (Red alien - 15 points):
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the green alien.")
+elif alien_color == 'yellow':
+    print("You just earned 10 points for shooting the yellow alien.")
+else:
+    print("You just earned 15 points for shooting the red alien.")
